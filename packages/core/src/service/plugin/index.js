feca0147ffd3165c254c6af94eb9c5c95eb259f4
Mohamed MHAMDI <mmhamdi@actualys.com>
2017-10-05 16:34:44 +0200

    add possibility to inject custom grid classes

--- packages/core/src/service/plugin/index.js
+++ packages/core/src/service/plugin/index.js
@@ -1,29 +1,29 @@
 // @flow
-import uuid from 'uuid'
-import { satisfies } from 'semver'
-import { ContentPlugin, LayoutPlugin, Plugin, NativePlugin } from './classes'
-import type { ComponetizedCell, NativeFactory } from '../../types/editable'
-import defaultPlugin from './default'
-import { layoutMissing, contentMissing } from './missing'
+import uuid from "uuid";
+import { satisfies } from "semver";
+import { ContentPlugin, LayoutPlugin, Plugin, NativePlugin } from "./classes";
+import type { ComponetizedCell, NativeFactory } from "../../types/editable";
+import defaultPlugin from "./default";
+import { layoutMissing, contentMissing } from "./missing";
 
-const find = (name: string, version: string = '*') => (
+const find = (name: string, version: string = "*") => (
   plugin: Plugin
-): boolean => plugin.name === name && satisfies(plugin.version, version)
+): boolean => plugin.name === name && satisfies(plugin.version, version);
 
 /**
  * Iterate through an editable content tree and generate ids where missing.
  */
 export const generateMissingIds = (props: Object): Object => {
-  const { rows, cells, id } = props
+  const { rows, cells, id } = props;
 
   if ((rows || []).length > 0) {
-    props.rows = rows.map(generateMissingIds)
+    props.rows = rows.map(generateMissingIds);
   } else if ((cells || []).length > 0) {
-    props.cells = cells.map(generateMissingIds)
+    props.cells = cells.map(generateMissingIds);
   }
 
-  return { ...props, id: id || uuid.v4() }
-}
+  return { ...props, id: id || uuid.v4() };
+};
 
 /**
  * PluginService is a registry of all content and layout plugins known to the editor.
@@ -33,7 +33,7 @@ export default class PluginService {
     content: Array<ContentPlugin>,
     layout: Array<LayoutPlugin>,
     native?: NativeFactory
-  }
+  };
 
   /**
    * Instantiate a new PluginService instance. You can provide your own set of content and layout plugins here.
@@ -51,86 +51,86 @@ export default class PluginService {
       ),
       layout: layout.map((config: any) => new LayoutPlugin(config)),
       native
-    }
+    };
   }
 
-  hasNativePlugin = () => Boolean(this.plugins.native)
+  hasNativePlugin = () => Boolean(this.plugins.native);
 
-  getNativePlugin = () => this.plugins.native
+  getNativePlugin = () => this.plugins.native;
 
   createNativePlugin = (
     hover: any,
     monitor: any,
     component: any
   ): ComponetizedCell => {
-    const native = this.plugins.native
+    const native = this.plugins.native;
 
     if (!native) {
-      const insert = new NativePlugin({})
-      const cell: any = { node: insert, rawNode: () => insert }
-      return cell
+      const insert = new NativePlugin({});
+      const cell: any = { node: insert, rawNode: () => insert };
+      return cell;
     }
 
-    const plugin = new NativePlugin(native(hover, monitor, component))
-    const initialState = plugin.createInitialState()
-    let insert: any = { content: { plugin, state: initialState } }
-    if (plugin === 'layout') {
-      insert = { layout: { plugin, state: initialState } }
+    const plugin = new NativePlugin(native(hover, monitor, component));
+    const initialState = plugin.createInitialState();
+    let insert: any = { content: { plugin, state: initialState } };
+    if (plugin === "layout") {
+      insert = { layout: { plugin, state: initialState } };
     }
 
-    const cell: any = { node: insert, rawNode: () => insert }
-    return cell
-  }
+    const cell: any = { node: insert, rawNode: () => insert };
+    return cell;
+  };
 
   setLayoutPlugins = (plugins: Array<any> = []) => {
-    this.plugins.layout = []
-    plugins.forEach((plugin: any) => this.addLayoutPlugin(plugin))
-  }
+    this.plugins.layout = [];
+    plugins.forEach((plugin: any) => this.addLayoutPlugin(plugin));
+  };
 
   addLayoutPlugin = (config: any) => {
-    this.plugins.layout.push(new LayoutPlugin(config))
-  }
+    this.plugins.layout.push(new LayoutPlugin(config));
+  };
 
   removeLayoutPlugin = (name: string) => {
     this.plugins.layout = this.plugins.layout.filter(
       (plugin: LayoutPlugin) => plugin.name !== name
-    )
-  }
+    );
+  };
 
   setContentPlugins = (plugins: Array<any> = []) => {
-    this.plugins.content = []
+    this.plugins.content = [];
 
     // semicolon is required to avoid syntax error
-    ;[defaultPlugin, ...plugins].forEach((plugin: any) =>
+    [defaultPlugin, ...plugins].forEach((plugin: any) =>
       this.addContentPlugin(plugin)
-    )
-  }
+    );
+  };
 
   addContentPlugin = (config: any) => {
-    this.plugins.content.push(new ContentPlugin(config))
-  }
+    this.plugins.content.push(new ContentPlugin(config));
+  };
 
   removeContentPlugin = (name: string) => {
     this.plugins.content = this.plugins.content.filter(
       (plugin: ContentPlugin) => plugin.name !== name
-    )
-  }
+    );
+  };
 
   /**
    * Finds a layout plugin based on its name and version.
    */
   findLayoutPlugin = (name: string, version: string): LayoutPlugin => {
-    const plugin = this.plugins.layout.find(find(name, version))
-    return plugin || new LayoutPlugin(layoutMissing({ name, version }))
-  }
+    const plugin = this.plugins.layout.find(find(name, version));
+    return plugin || new LayoutPlugin(layoutMissing({ name, version }));
+  };
 
   /**
    * Finds a content plugin based on its name and version.
    */
   findContentPlugin = (name: string, version: string): ContentPlugin => {
-    const plugin = this.plugins.content.find(find(name, version))
-    return plugin || new ContentPlugin(contentMissing({ name, version }))
-  }
+    const plugin = this.plugins.content.find(find(name, version));
+    return plugin || new ContentPlugin(contentMissing({ name, version }));
+  };
 
   /**
    * Returns a list of all known plugin names.
@@ -138,7 +138,7 @@ export default class PluginService {
   getRegisteredNames = (): Array<string> => [
     ...this.plugins.content.map(({ name }: Plugin) => name),
     ...this.plugins.layout.map(({ name }: Plugin) => name)
-  ]
+  ];
 
   unserialize = (state: any): Object => {
     const {
@@ -148,74 +148,75 @@ export default class PluginService {
       layout = {},
       inline,
       size,
+      classNames,
       id
-    } = state
-    const newState: Object = { id, inline, size }
+    } = state;
+    const newState: Object = { id, inline, classNames, size };
 
     const {
-      plugin: { name: contentName = null, version: contentVersion = '*' } = {},
+      plugin: { name: contentName = null, version: contentVersion = "*" } = {},
       state: contentState
     } =
-      content || {}
+      content || {};
     const {
-      plugin: { name: layoutName = null, version: layoutVersion = '*' } = {},
+      plugin: { name: layoutName = null, version: layoutVersion = "*" } = {},
       state: layoutState
     } =
-      layout || {}
+      layout || {};
 
     if (contentName) {
-      const plugin = this.findContentPlugin(contentName, contentVersion)
+      const plugin = this.findContentPlugin(contentName, contentVersion);
       newState.content = {
         plugin,
         state: plugin.unserialize(contentState)
-      }
+      };
     }
 
     if (layoutName) {
-      const plugin = this.findLayoutPlugin(layoutName, layoutVersion)
+      const plugin = this.findLayoutPlugin(layoutName, layoutVersion);
       newState.layout = {
         plugin,
         state: plugin.unserialize(layoutState)
-      }
+      };
     }
 
     if ((rows || []).length) {
-      newState.rows = rows.map(this.unserialize)
+      newState.rows = rows.map(this.unserialize);
     }
 
     if ((cells || []).length) {
-      newState.cells = cells.map(this.unserialize)
+      newState.cells = cells.map(this.unserialize);
     }
 
-    return generateMissingIds(newState)
-  }
+    return generateMissingIds(newState);
+  };
 
   serialize = (state: Object): Object => {
-    const { rows = [], cells = [], content, layout, inline, size, id } = state
+    const { rows = [], cells = [], content, layout, inline, size, id } = state;
 
-    const newState: Object = { id, inline, size }
+    const newState: Object = { id, inline, size };
     if (content && content.plugin) {
       newState.content = {
         plugin: { name: content.plugin.name, version: content.plugin.version },
         state: content.plugin.serialize(content.state)
-      }
+      };
     }
 
     if (layout && layout.plugin) {
       newState.layout = {
         plugin: { name: layout.plugin.name, version: layout.plugin.version },
         state: layout.plugin.serialize(layout.state)
-      }
+      };
     }
 
     if (rows.length) {
-      newState.rows = rows.map(this.serialize)
+      newState.rows = rows.map(this.serialize);
     }
 
     if (cells.length) {
-      newState.cells = cells.map(this.serialize)
+      newState.cells = cells.map(this.serialize);
     }
 
-    return newState
-  }
+    return newState;
+  };
 }
--- packages/renderer/src/index.js
+++ packages/renderer/src/index.js
@@ -1,25 +1,28 @@
 // @flow
-import React from 'react'
-import classNames from 'classnames'
-import PluginService from 'ory-editor-core/lib/service/plugin'
-import { editable as reducer } from 'ory-editor-core/lib/reducer/editable'
-import type { Cell, Row } from 'ory-editor-core/lib/types/editable'
+import React from "react";
+import classNames from "classnames";
+import PluginService from "ory-editor-core/lib/service/plugin";
+import { editable as reducer } from "ory-editor-core/lib/reducer/editable";
+import type { Cell, Row } from "ory-editor-core/lib/types/editable";
 
-const gridClass = (size: number = 12): string =>
-  `ory-cell-sm-${size} ory-cell-xs-12`
+// if classNames is set, default size is for md devices, the rest of devices is classNames responsibility
+const gridClass = (size: number = 12, classNames: ?string = null): string =>
+  classNames
+    ? classNames.replace("<size>", size)
+    : `ory-cell-sm-${size} ory-cell-xs-12`;
 
 const HTMLRow = ({ cells = [], className, hasInlineChildren }: Row) => (
   <div
-    className={classNames('ory-row', className, {
-      'ory-row-has-floating-children': hasInlineChildren
+    className={classNames("ory-row", className, {
+      "ory-row-has-floating-children": hasInlineChildren
     })}
   >
     {cells.map((c: Cell) => <HTMLCell key={c.id} {...c} />)}
   </div>
-)
+);
 
 // eslint-disable-next-line no-empty-function
-const noop = () => {}
+const noop = () => {};
 
 const HTMLCell = (props: Cell) => {
   const {
@@ -28,15 +31,16 @@ const HTMLCell = (props: Cell) => {
     content = {},
     hasInlineNeighbour,
     inline,
-    size
-  } = props
-  const cn = classNames('ory-cell', gridClass(size), {
-    'ory-cell-has-inline-neighbour': hasInlineNeighbour,
-    [`ory-cell-inline-${inline || ''}`]: inline
-  })
+    size,
+    classNames: gridClassNames
+  } = props;
+  const cn = classNames("ory-cell", gridClass(size, gridClassNames), {
+    "ory-cell-has-inline-neighbour": hasInlineNeighbour,
+    [`ory-cell-inline-${inline || ""}`]: inline
+  });
 
   if (layout.plugin) {
-    const { state, plugin: { Component } } = layout
+    const { state, plugin: { Component } } = layout;
 
     return (
       <div className={cn}>
@@ -48,10 +52,10 @@ const HTMLCell = (props: Cell) => {
           </Component>
         </div>
       </div>
-    )
+    );
   } else if (content.plugin) {
-    const { state, plugin: { Component, StaticComponent } } = content
-    const Renderer = StaticComponent || Component
+    const { state, plugin: { Component, StaticComponent } } = content;
+    const Renderer = StaticComponent || Component;
 
     return (
       <div className={cn}>
@@ -59,7 +63,7 @@ const HTMLCell = (props: Cell) => {
           <Renderer isPreviewMode readOnly state={state} onChange={noop} />
         </div>
       </div>
-    )
+    );
   } else if (rows.length > 0) {
     return (
       <div className={cn}>
@@ -67,15 +71,15 @@ const HTMLCell = (props: Cell) => {
           <HTMLRow key={r.id} {...r} className="ory-cell-inner" />
         ))}
       </div>
-    )
+    );
   }
 
   return (
     <div className={cn}>
       <div className="ory-cell-inner" />
     </div>
-  )
-}
+  );
+};
 
 export const HTMLRenderer = ({
   state,
@@ -84,8 +88,9 @@ export const HTMLRenderer = ({
   state: any,
   plugins: { layout: [], content: [] }
 }) => {
-  const service = new PluginService(plugins)
-  const props = reducer(service.unserialize(state), { type: 'renderer/noop' })
+  const service = new PluginService(plugins);
 
-  return <HTMLRow {...props} />
-}
+  console.log(service.unserialize(state));
+  const props = reducer(service.unserialize(state), { type: "renderer/noop" });
+  return <HTMLRow {...props} />;
+};
